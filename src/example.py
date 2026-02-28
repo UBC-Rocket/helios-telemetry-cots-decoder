b2ba6cd672377d2e063f8412440ef9ff2c3c477c
Jason <jasonkwok475@gmail.com>
2026-02-27 18:55:27 -0800

    Added example code

---
+++ src/example.py
@@ -0,0 +1,27 @@
+import time
+import helios_python as helios
+from generated.python.transport.packet_pb2 import TransportPacket
+from google.protobuf.timestamp_pb2 import Timestamp
+
+print("Hello from rocketdecoder container!")
+
+with helios.HeliosClient(retry=999, retry_delay=5.0) as client:
+    print("Connected to Helios!")
+
+    packet = TransportPacket(
+        id=1,
+        timestamp=Timestamp(seconds=int(time.time())),
+        address="rocketdecoder",
+        data=b"my raw payload bytes",
+    )
+    reply: TransportPacket = client.send_recv(packet, TransportPacket)
+    print(f"rocketdecoder - Received from server: {reply.data}")
+
+    while True:
+        msg: TransportPacket = client.recv(TransportPacket)
+        if msg is None:
+            print("Connection closed by peer.")
+            break
+
+        print(f"rocketdecoder - Received: {msg.data}")
+        time.sleep(5)
